438bc1c3147d0dd4d0301a5ae1f82a6207e4e02c
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2020-10-29 21:21:24 -0300

    Projeto criado

---
+++ backend/src/main/java/io/github/guisofiati/OngprojectApplication.java
@@ -0,0 +1,13 @@
+package io.github.guisofiati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngprojectApplication.class, args);
+	}
+
+}
