f9c349450cf6a229453597e01af4278e7c3db696
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2020-11-18 14:47:45 -0300

    Endereco e associacoes

--- backend/src/main/java/io/github/guisofiati/domain/Cliente.java
+++ backend/src/main/java/io/github/guisofiati/domain/Cliente.java
@@ -1,12 +1,19 @@
 package io.github.guisofiati.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -29,6 +36,13 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@OneToMany(mappedBy= "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name= "tb_telefone")
+	private Set<String> telefones = new HashSet<>(); 
 	
 	@OneToOne
 	@JoinColumn(name = "trabalho_id")
